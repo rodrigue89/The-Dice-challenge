bfb46eda2c0c2b7e6218cfca61e7ce4e1b8922b3
Rodrigue Guissou <guissourodrigue@yahoo.fr>
2019-09-22 19:06:17 -0400

    Initial Start up files

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() *6) +1 ;
+var randomDiceImage = "Dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+// the second way to roll the second dice.
+
+var randomNumber2 = Math.floor(Math.random() *6) +1
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// for player 1 to win
+if (randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").innerHTML = " Player 1 Wins !!! ";
+}
+// for player 2 to win
+else if (randomNumber2 > randomNumber1 ) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins !!!";
+}
+
+else {
+  document.querySelector("h1").innerHTML = " Draw!!";
+}
